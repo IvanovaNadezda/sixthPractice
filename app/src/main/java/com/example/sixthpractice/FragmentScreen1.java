df0acaf2d66361c2c9c6c1f7ce8336f66a46c231
inade <inadezda2003@gmail.com>
2023-03-23 22:04:13 +0300

    Добавление кода уведомления

--- app/src/main/java/com/example/sixthpractice/FragmentScreen1.java
+++ app/src/main/java/com/example/sixthpractice/FragmentScreen1.java
@@ -1,7 +1,12 @@
 package com.example.sixthpractice;
 
 
-import android.annotation.SuppressLint;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -9,18 +14,20 @@ import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
-import androidx.navigation.Navigation;
 
 import com.example.sixthpractice.databinding.Screen1Binding;
 
-
 public class FragmentScreen1 extends Fragment {
     Screen1Binding binding;
-    float ratingOwl;
-    float ratingFodder;
-    public FragmentScreen1()
-    {
+
+    private static final String CHANNEL = "CHANNEL";
+    private static final int NOTIFICATION_ID = 1;
+
+    public FragmentScreen1() {
         super(R.layout.screen1);
     }
 
@@ -31,41 +38,55 @@ public class FragmentScreen1 extends Fragment {
         return binding.getRoot();
     }
 
-    @SuppressLint("SetTextI18n")
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        Bundle bundle = getArguments();
-        if (bundle != null) {
-            if (bundle.containsKey("rating2"))
-            {
-                // Устанавливаем данные из второго фрагмента
-                ratingOwl = bundle.getFloat("rating2");
-                binding.textView14.setText("Вы выбрали сову с рейтингом "+ ratingOwl);
-            }
-            else
-            {
-                // Устанавливаем данные из третьего фрагмента
-                ratingFodder = bundle.getFloat("rating3");
-                binding.textView6.setText("Вы выбрали корм с рейтингом "+ ratingFodder);
-            }
-
+        // Создаем канал уведомлений
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence name = "МОЙ КАНАЛ";
+            String description = "КАНАЛ ДЛЯ УВЕДОМЛЕНИЙ";
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel = new NotificationChannel(CHANNEL, name, importance);
+            channel.setDescription(description);
+            NotificationManager notificationManager = requireContext().
+                    getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
         }
+        // Реакция на уведомления
+        Intent notificationIntent = new Intent(getActivity(), MainActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(getActivity(),
+                0, notificationIntent,
+                PendingIntent.FLAG_CANCEL_CURRENT);
 
-        // Переход на второй фрагмент из первого
-        binding.button4.setOnClickListener(new View.OnClickListener() {
+        binding.imageButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Navigation.findNavController(view).navigate(R.id.action_first_fragment_to_second_fragment);
+
+                // Есть ли разрешения на отправку уведомления
+                if (ContextCompat.checkSelfPermission(getActivity(), android.Manifest.permission.
+                        POST_NOTIFICATIONS) != PackageManager.PERMISSION_GRANTED) {
+                    // Если разрешение не получено, запрашиваем его у пользователя
+                    requestPermissions(new String[]{android.Manifest.permission.POST_NOTIFICATIONS}, 1);
+                }
+                // Создаем уведомление
+                NotificationCompat.Builder builder = new NotificationCompat.Builder(requireContext(),
+                        CHANNEL)
+                        .setSmallIcon(R.drawable.owl)
+                        .setContentText("Вы не забыли покормить сову?")
+                        .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+                // Отправляем уведомление
+                NotificationManagerCompat notificationManager = NotificationManagerCompat.from(requireContext());
+                notificationManager.notify(NOTIFICATION_ID, builder.build());
             }
-        });
-        // Переход на третий фрагмент из первого
-        binding.button6.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Navigation.findNavController(view).navigate(R.id.action_first_fragment_to_third_fragment);
-            }
+
+
         });
 
+    }
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(
+                requestCode, permissions, grantResults
+        );
     }
 }
